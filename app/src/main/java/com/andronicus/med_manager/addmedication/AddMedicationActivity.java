83d70248ddf84f461d6c36d734b7b00c7ce6b84e
andronicus-kim <androkim.kim7@gmail.com>
2018-03-31 16:33:28 +0300

    adds addmedicationfragment

--- app/src/main/java/com/andronicus/med_manager/addmedication/AddMedicationActivity.java
+++ app/src/main/java/com/andronicus/med_manager/addmedication/AddMedicationActivity.java
@@ -3,11 +3,13 @@ package com.andronicus.med_manager.addmedication;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.andronicus.med_manager.R;
+import com.andronicus.med_manager.util.ActivityUtil;
 
 public class AddMedicationActivity extends AppCompatActivity {
     private static final String TAG = "AddMedicationActivity";
@@ -26,6 +28,12 @@ public class AddMedicationActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
         }catch (NullPointerException e){
             Log.e(TAG, "onCreate: " + e.getMessage() );
+        }
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = AddMedicationFragment.newInstance();
+            ActivityUtil.addFragmentToActivity(getSupportFragmentManager(),R.id.fragment_container,fragment);
         }
     }
 }
---
+++ app/src/main/java/com/andronicus/med_manager/addmedication/AddMedicationFragment.java
@@ -0,0 +1,29 @@
+package com.andronicus.med_manager.addmedication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andronicus.med_manager.R;
+
+public class AddMedicationFragment extends Fragment {
+
+    public static AddMedicationFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        AddMedicationFragment fragment = new AddMedicationFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_medication, container, false);
+    }
+
+}
