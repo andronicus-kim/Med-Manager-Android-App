34b56426cbbab57999353695fced68a377529752
andronicus-kim <androkim.kim7@gmail.com>
2018-03-31 22:13:45 +0300

    customizes edit medication layout

--- app/src/main/java/com/andronicus/med_manager/editmedication/EditMedicationFragment.java
+++ app/src/main/java/com/andronicus/med_manager/editmedication/EditMedicationFragment.java
@@ -10,13 +10,32 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.andronicus.med_manager.R;
 import com.andronicus.med_manager.medication.MedicationActivity;
+import com.andronicus.med_manager.util.DatePickerFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class EditMedicationFragment extends Fragment {
 
+
+    private Unbinder mUnbinder;
+    @BindView(R.id.et_name)
+    EditText mEditTextName;
+    @BindView(R.id.et_description)
+    EditText mEditTextDescription;
+    @BindView(R.id.et_frequency)
+    EditText mEditTextFrequency;
+    @BindView(R.id.et_start_date)
+    EditText mEditTextStartDate;
+    @BindView(R.id.et_end_date)
+    EditText mEditTextEndDate;
 
     public static EditMedicationFragment newInstance() {
 
@@ -37,7 +56,19 @@ public class EditMedicationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_medication, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_medication, container, false);
+        mUnbinder = ButterKnife.bind(this,view);
+        return view;
+    }
+    @OnClick(R.id.et_start_date) public void onStartDateClick(){
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.passClickedEditText(mEditTextStartDate);
+        datePickerFragment.show(getActivity().getSupportFragmentManager(),"date-picker");
+    }
+    @OnClick(R.id.et_end_date) public void onEndDateClick(){
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.passClickedEditText(mEditTextEndDate);
+        datePickerFragment.show(getActivity().getSupportFragmentManager(),"date-picker");
     }
 
     @Override
@@ -49,9 +80,30 @@ public class EditMedicationFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_save){
-            Toast.makeText(getActivity(), "Updating...", Toast.LENGTH_SHORT).show();
+            if (mEditTextName.getText().toString().equals("")){
+                mEditTextName.setError("Name cannot be Blank!");
+                return false;
+            }else if (mEditTextDescription.getText().toString().equals("")){
+                mEditTextDescription.setError("Description cannot be Blank!");
+                return false;
+            }else if (mEditTextFrequency.getText().toString().equals("")){
+                mEditTextFrequency.setError("Frequency cannot be Blank!");
+                return false;
+            }else if (mEditTextStartDate.getText().toString().equals("")){
+                mEditTextStartDate.setError("Start date cannot be Blank!");
+                return false;
+            }else if (mEditTextEndDate.getText().toString().equals("")){
+                mEditTextEndDate.setError("End date cannot be Blank!");
+                return false;
+            }
+            Toast.makeText(getActivity(), "Saving...", Toast.LENGTH_SHORT).show();
             startActivity(MedicationActivity.newIntent(getActivity()));
         }
         return true;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
     }
 }
