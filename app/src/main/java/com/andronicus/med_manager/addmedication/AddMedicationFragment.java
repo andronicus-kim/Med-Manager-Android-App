7e858663f7ddd120421732415c3a875cc1b16c40
andronicus-kim <androkim.kim7@gmail.com>
2018-03-31 17:10:17 +0300

    sets up add medication fragment and menu

--- app/src/main/java/com/andronicus/med_manager/addmedication/AddMedicationFragment.java
+++ app/src/main/java/com/andronicus/med_manager/addmedication/AddMedicationFragment.java
@@ -2,12 +2,18 @@ package com.andronicus.med_manager.addmedication;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.andronicus.med_manager.R;
+import com.andronicus.med_manager.medication.MedicationActivity;
 
 public class AddMedicationFragment extends Fragment {
 
@@ -19,6 +25,13 @@ public class AddMedicationFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,4 +39,18 @@ public class AddMedicationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_medication, container, false);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.add_medication,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save){
+            Toast.makeText(getActivity(), "Saving...", Toast.LENGTH_SHORT).show();
+            startActivity(MedicationActivity.newIntent(getActivity()));
+        }
+        return true;
+    }
 }
