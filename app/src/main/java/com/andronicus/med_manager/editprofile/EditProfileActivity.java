3c972635d1a07894112111c30919863701fecf2f
andronicus-kim <androkim.kim7@gmail.com>
2018-04-02 17:33:19 +0300

    sets up image selection

--- app/src/main/java/com/andronicus/med_manager/editprofile/EditProfileActivity.java
+++ app/src/main/java/com/andronicus/med_manager/editprofile/EditProfileActivity.java
@@ -1,15 +1,35 @@
 package com.andronicus.med_manager.editprofile;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.andronicus.med_manager.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,9 +37,15 @@ import butterknife.Unbinder;
 
 public class EditProfileActivity extends AppCompatActivity {
     private static final String TAG = "EditProfileActivity";
+    private static final int RC_LOAD_IMAGE = 200;
 
 
     private Unbinder mUnbinder;
+    private Uri mUri;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseStorage mStorage;
+    private String mUserId;
     @BindView(R.id.iv_profile_pic)
     ImageView mImageViewProfilePic;
     @BindView(R.id.et_name)
@@ -36,12 +62,72 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         try{
-            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }catch (NullPointerException e){
             Log.e(TAG, "onCreate: " + e.getMessage() );
         }
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null){
+            mUserId = mAuth.getCurrentUser().getUid();
+        }
         mUnbinder = ButterKnife.bind(this);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(mUserId);
+        mStorage = FirebaseStorage.getInstance();
 
+        mImageViewProfilePic.setOnClickListener((view) -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent,RC_LOAD_IMAGE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_LOAD_IMAGE && resultCode == Activity.RESULT_OK){
+            mUri = data.getData();
+            mImageViewProfilePic.setImageURI(mUri);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_profile,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_save){
+            if (mUri != null){
+                StorageReference filePath = mStorage.getReference().child("profile_images").child(mUserId);
+                Bitmap bitmap = null;
+                try{
+                    bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(),mUri);
+                }catch (IOException e){
+                    Log.e(TAG, "onOptionsItemSelected: " + e.getMessage());
+                }
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG,20,byteArrayOutputStream);
+                byte[] data = byteArrayOutputStream.toByteArray();
+                UploadTask uploadTask = filePath.putBytes(data);
+                uploadTask.addOnSuccessListener(taskSnapshot -> {
+                   Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Map<String,Object> newPost = new HashMap<>();
+                    newPost.put("profileImageUrl",downloadUrl.toString());
+                    /*
+                    * Save the download url to database
+                    * */
+                    mDatabaseReference.updateChildren(newPost);
+                    finish();
+                });
+                uploadTask.addOnFailureListener(e -> Toast.makeText(EditProfileActivity.this, "Error uploading Image!", Toast.LENGTH_SHORT).show());
+            }else {
+                finish();
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
