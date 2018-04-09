e3503c334ce84f36fdea2b8856730f558854e68d
andronicus-kim <androkim.kim7@gmail.com>
2018-04-10 00:15:07 +0300

    triggers notification when alarm goes off

--- app/src/main/java/com/andronicus/med_manager/util/AlarmReceiver.java
+++ app/src/main/java/com/andronicus/med_manager/util/AlarmReceiver.java
@@ -1,18 +1,50 @@
 package com.andronicus.med_manager.util;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.andronicus.med_manager.R;
+import com.andronicus.med_manager.medication.MedicationActivity;
 
 /**
  * Created by andronicus on 4/9/2018.
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
+    private static final String TAG = "AlarmReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Alarm Received!", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "Alarm received!");
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        Intent contentIntent = new Intent(context, MedicationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity
+                (context, 0, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.small_icon)
+                .setContentTitle("MedManager")
+                .setContentText("Take your medication")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        //Deliver the notification
+        notificationManager.notify(0, builder.build());
     }
 }
