aa510d85e8db3a4c1974f40a4447bc202d81c5c1
andronicus-kim <androkim.kim7@gmail.com>
2018-04-09 22:48:09 +0300

    displays log message when alarm is received

--- app/src/main/java/com/andronicus/med_manager/util/AlarmReceiver.java
+++ app/src/main/java/com/andronicus/med_manager/util/AlarmReceiver.java
@@ -3,17 +3,16 @@ package com.andronicus.med_manager.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by andronicus on 4/9/2018.
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
-    private static final String TAG = "AlarmReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.e(TAG, "Alarm received!");
+        Toast.makeText(context, "Alarm Received!", Toast.LENGTH_SHORT).show();
     }
 }
